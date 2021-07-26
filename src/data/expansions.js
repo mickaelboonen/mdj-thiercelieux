e98a1939e9d61dd9257886785abd9687d23248d1
Lestrarcher <mickaelboonen@gmail.com>
2021-07-26 09:50:43 +0200

    config et data

---
+++ src/data/expansions.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/prefer-default-export
+export const expansions = [
+  {
+    id: 1,
+    name: 'Loup-Garou de Thiercelieux',
+    description: '',
+  },
+  {
+    id: 2,
+    name: 'Nouvelle Lune',
+    description: '',
+  },
+  {
+    id: 3,
+    name: 'Personnages',
+    description: '',
+  },
+  {
+    id: 4,
+    name: 'Le Village',
+    description: '',
+  },
+
+];
