33cf952875f159515e554ebf10db72b1346cd859
Lestrarcher <mickaelboonen@gmail.com>
2021-08-02 13:19:32 +0200

    installed React Router DOM

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 // == Import npm
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Header from 'src/components/App/Header';
 import Home from 'src/components/App/Home';
@@ -10,7 +11,11 @@ const App = () => (
   <div className="app">
     <Header />
     <main>
-      <Home />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
     </main>
     <Footer />
   </div>
