c6bb0facb3985368b945b81e7a5757786c129f6f
christal-chambost <chambost.christal@gmail.com>
2021-08-06 10:12:02 +0200

    link of the page about us in the footer

--- src/components/App/Footer/index.js
+++ src/components/App/Footer/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { NavLink } from 'react-router-dom';
+
 import './style.scss';
 
 const Footer = () => (
-  <div>Footer</div>
+  <div className="footer">
+    <nav className="footer__navbar">
+      <ul className="footer__navbar-list">
+        <li>
+          <NavLink
+            to="/qui-sommes-nous"
+          >
+            Qui sommes-nous
+          </NavLink>
+          </li>
+        <li>Contactez nous</li>
+      </ul>
+    </nav>
+  </div>
 );
 
 Footer.propTypes = {
