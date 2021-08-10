4ea55d94ea2f5249c5415059815c68268ab1758e
Lestrarcher <mickaelboonen@gmail.com>
2021-08-10 18:47:57 +0200

    new commit for the merge problem

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,12 +12,14 @@ import Game from 'src/containers/App/Expansions/Game';
 import Specificity from 'src/containers/App/Expansions/Specificity';
 
 import Roles from 'src/containers/App/Roles';
-import NewMoon from 'src/containers/App/NewMoon';
-
 import Expansions from 'src/containers/App/Expansions';
 import Aboutus from 'src/containers/App/Aboutus';
+import NewMoon from 'src/containers/App/NewMoon';
+
 import ProfileUser from './ProfileUser';
 import Mailbox from './ProfileUser/Mailbox';
+
+// Free comment
 
 // == Composant
 const App = () => (
