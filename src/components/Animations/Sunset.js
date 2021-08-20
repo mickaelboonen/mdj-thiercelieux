123ff5b5892019dbcbb89d90855364cb5781c36d
Lestrarcher <mickaelboonen@gmail.com>
2021-08-20 16:55:19 +0200

    created component Game + setTimeOut on the Sunset component

--- src/components/Animations/Sunset.js
+++ src/components/Animations/Sunset.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import './style.scss';
+import { useHistory } from 'react-router-dom';
 
-const Sunset = () => (
-  <div className="sunset">Sunset</div>
-);
+const Sunset = () => {
+  const history = useHistory();
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/partie-en-cours');
+    }, 5000);
+  }, []);
+  return (
+    <div className="sunset">Sunset</div>
+  );
+};
 
 Sunset.propTypes = {
 
---
+++ src/components/App/CurrentGame/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.scss';
+
+const Game = () => (
+  <div className="current-game">Game</div>
+);
+
+Game.propTypes = {
+
+};
+
+export default Game;
