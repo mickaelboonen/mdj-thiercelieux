96f62685bd4673e759d827d3951246dc6b3e5f93
Lestrarcher <mickaelboonen@gmail.com>
2021-08-27 15:04:48 +0200

    began adding new message but left off to see websockets his WE

--- src/containers/App/Mailbox/MessagePrive/index.js
+++ src/containers/App/Mailbox/MessagePrive/index.js
@@ -1,10 +1,16 @@
 import { connect } from 'react-redux';
 import MessagePrive from 'src/components/App/ProfileUser/Mailbox/MessagePrive';
 
+import { addNewMessage } from 'src/actions/mailbox';
+
 const mapStateToProps = (state) => ({
   conversations: state.mails.conversations,
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = (dispatch) => ({
+  addNewMessage: (data) => {
+    dispatch(addNewMessage(data));
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(MessagePrive);
