074d6501efb9490a155ea92dcb75b1690866d935
mickaelboonen <mickaelboonen@gmail.com>
2021-11-04 03:38:24 +0100

    added donut chart to the stats pages + stats functions + foreach in scss

--- src/components/App/ProfileUser/Stats/index.js
+++ src/components/App/ProfileUser/Stats/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ChartDonut } from '@patternfly/react-charts';
 
 import {
   setWinningStats,
   setDeathStats,
+  setVillageSideRolesStats,
+  setVillageStats,
+  setWerewolfSideRolesStats,
+  setSoloSideRolesStats,
+  setAmbiguousSideRolesStats,
+  setOtherRolesStats,
+  setChartData,
 } from 'src/selectors/statsFunctions';
 
 import './style.scss';
 
-const Stats = ({ stats, fetchStatsForProfile }) => {
-  // useEffect(() => {
-  //   fetchStatsForProfile();
-  // }, []);
-  console.log(stats);
+const Stats = ({ stats }) => {
   const keys = Object.keys(stats);
 
   const winningStats = keys.filter((key) => key.includes('won'));
@@ -24,12 +28,21 @@ const Stats = ({ stats, fetchStatsForProfile }) => {
     || key.includes('won')
     || key.includes('parties')
     || key.includes('id')
-    || key.includes('at')) {
+    || key.includes('ed_at')) {
       return;
     }
     return key;
   });
   const filteredRolesStats = rolesStats.filter((key) => key !== undefined);
+  const rolesStatsArray = setVillageSideRolesStats(filteredRolesStats, stats);
+  const villagesStatsArray = setVillageStats(rolesStats, stats);
+  const werewolfSideRolesArray = setWerewolfSideRolesStats(filteredRolesStats, stats);
+  const soloSideRolesArray = setSoloSideRolesStats(filteredRolesStats, stats);
+  const ambiguousSideRolesArray = setAmbiguousSideRolesStats(filteredRolesStats, stats);
+  const otherRolesArray = setOtherRolesStats(filteredRolesStats, stats);
+  const array = [rolesStatsArray, soloSideRolesArray, ambiguousSideRolesArray, werewolfSideRolesArray];
+  console.log(array);
+  const chartData = setChartData(array, stats.played_parties);
   return (
     <div className="stats">
       <h3>Stats</h3>
@@ -37,7 +50,7 @@ const Stats = ({ stats, fetchStatsForProfile }) => {
         <h5 className="stats__category-title">NOMBRES DE PARTIES :</h5>
         <ul className="stats__category-list">
           <li className="stats__category-list-item">Animées: {stats.hosted_parties}</li>
-          <li className="stats__category-list-item">Jouées : {stats.played_parties}</li>
+          <li className="stats__category-list-item">Jouées: {stats.played_parties}</li>
         </ul>
       </div>
       <div className="stats__category">
@@ -58,17 +71,76 @@ const Stats = ({ stats, fetchStatsForProfile }) => {
       </div>
       <div className="stats__category">
         <h5 className="stats__category-title">STATISTIQUES PAR ROLES CACHÉS :</h5>
-        <ul className="stats__category-list">
-          {filteredRolesStats.map((key) => (
-            <li className="stats__category-list-item">{key}: {stats[key]}</li>
-          ))}
-        </ul>
+        <div className="stats__category-list stats__category-list--hidden-roles">
+          <div className="stats__category-chart">
+            {/* {otherRolesArray.map((key) => (
+              <li className="stats__category-list-item">{key[0]}: {key[1]}</li>
+            ))} */}
+            <ChartDonut
+              ariaDesc="Average number of pets"
+              ariaTitle="Donut chart example"
+              constrainToVisibleArea={true}
+              data={// chartData
+                [
+                  { x: 'Village', y: 4 },
+                  { x: 'Loup-Garou', y: 10 },
+                  { x: 'Ambigü', y: 2 },
+                  { x: 'Solitaire', y: 1 },
+                ]
+              }
+              height={200}
+              labels={({ datum }) => `${datum.x}: ${datum.y}%`}
+              // legendData={[
+              //   { name: `Village: ${rolesStatsArray[rolesStatsArray.length - 1][1]}` },
+              //   { name: `Loup-Garou: ${werewolfSideRolesArray[werewolfSideRolesArray.length - 1][1]}` },
+              //   { name: `Ambigü: ${ambiguousSideRolesArray[ambiguousSideRolesArray.length - 1][1]}` },
+              //   { name: `Solitaire: ${soloSideRolesArray[soloSideRolesArray.length - 1][1]}` },
+              // ]}
+              // legendPosition="bottom"
+              // padding={{
+              //   bottom: 70, // Adjusted to accommodate legend
+              //   left: 20,
+              //   right: 50, // Adjusted to accommodate subTitle
+              //   top: 20,
+              // }}
+              subTitle="Pets"
+              subTitlePosition="center"
+              title=""
+              width={400}
+            />
+            <div>
+              {chartData.map((data) => (
+                <p className={`p-${data.color}`}>{data.x} : {data.y}</p>
+              ))}
+            </div>
+          </div>
+          <ul className="stats__category-list__sublist">
+            {rolesStatsArray.map((key) => (
+              <li className="stats__category-list-item">{key[0]}: {key[1]}</li>
+            ))}
+          </ul>
+          <ul className="stats__category-list__sublist">
+            {werewolfSideRolesArray.map((key) => (
+              <li className="stats__category-list-item">{key[0]}: {key[1]}</li>
+            ))}
+          </ul>
+          <ul className="stats__category-list__sublist">
+            {soloSideRolesArray.map((key) => (
+              <li className="stats__category-list-item">{key[0]}: {key[1]}</li>
+            ))}
+          </ul>
+          <ul className="stats__category-list__sublist">
+            {ambiguousSideRolesArray.map((key) => (
+              <li className="stats__category-list-item">{key[0]}: {key[1]}</li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="stats__category">
         <h5 className="stats__category-title">STATISTIQUES PAR ROLES DU VILLAGE :</h5>
         <ul className="stats__category-list">
-          {filteredRolesStats.map((key) => (
-            <li className="stats__category-list-item">{key}: {stats[key]}</li>
+          {villagesStatsArray.map((key) => (
+            <li className="stats__category-list-item">{key[0]}: {key[1]}</li>
           ))}
         </ul>
       </div>
@@ -77,7 +149,6 @@ const Stats = ({ stats, fetchStatsForProfile }) => {
 };
 
 Stats.propTypes = {
-  fetchStatsForProfile: PropTypes.func.isRequired,
   stats: PropTypes.object.isRequired,
 };
 
--- src/selectors/statsFunctions.js
+++ src/selectors/statsFunctions.js
@@ -54,3 +54,247 @@ export const setDeathStats = (keyArray, stats) => {
   newStatsArray = Object.entries(newStatsArray);
   return newStatsArray;
 };
+
+export const setVillageStats = (keyArray, stats) => {
+  let newStatsArray = {};
+  keyArray.forEach((key) => {
+    if (key === 'farmer') {
+      newStatsArray['Le Fermier'] = stats[key];
+    }
+    else if (key === 'priest') {
+      newStatsArray['Le Confesseur'] = stats[key];
+    }
+    else if (key === 'baker') {
+      newStatsArray['Le Boulanger'] = stats[key];
+    }
+    else if (key === 'mistress') {
+      newStatsArray["L'Institutrice"] = stats[key];
+    }
+    else if (key === 'bartender') {
+      newStatsArray['Le Tavernier'] = stats[key];
+    }
+    else if (key === 'barber') {
+      newStatsArray['Le Barbier'] = stats[key];
+    }
+    else if (key === 'bonesettler') {
+      newStatsArray['Le Rebouteux'] = stats[key];
+    }
+    else if (key === 'lord') {
+      newStatsArray['Le Châtelain'] = stats[key];
+    }
+    else if (key === 'bailiff') {
+      newStatsArray['Le Bailli'] = stats[key];
+    }
+    else if (key === 'vagabond') {
+      newStatsArray['Le Vagabond'] = stats[key];
+    }
+    delete newStatsArray[key];
+  });
+  newStatsArray = Object.entries(newStatsArray);
+  return newStatsArray;
+};
+
+export const setVillageSideRolesStats = (keyArray, stats) => {
+  let newStatsArray = {};
+  let totalRoles = 0;
+  keyArray.forEach((key) => {
+    if (key === 'hunter') {
+      newStatsArray['Le Chasseur'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'little_girl') {
+      newStatsArray['La Petite Fille'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'villager') {
+      newStatsArray['Le Simple Villageois'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'witch') {
+      newStatsArray['La Sorcière'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'seer') {
+      newStatsArray['La Voyante'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'ancient') {
+      newStatsArray["L'Ancien"] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'scapegoat') {
+      newStatsArray['Le Bouc Émissaire'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'guard') {
+      newStatsArray['Le Salvateur'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'villager_villager') {
+      newStatsArray['Le Villageois Villageois'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'two_sisters') {
+      newStatsArray['Les Deux Soeurs'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'three_brothers') {
+      newStatsArray['Les Trois Frères'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'stuttering_judge') {
+      newStatsArray['Le Juge Bègue'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'knight') {
+      newStatsArray["Le Chevalier à l'Épée Rouillée"] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'fox') {
+      newStatsArray['Le Renard'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'bear_tamer') {
+      newStatsArray["Le Montreur d'Ours"] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'gypsy') {
+      newStatsArray['La Gitane'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'raven') {
+      newStatsArray['Le Corbeau'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'pyromaniac') {
+      newStatsArray['Le Pyromane'] = stats[key];
+      totalRoles += stats[key];
+    }
+    delete newStatsArray[key];
+  });
+  newStatsArray = Object.entries(newStatsArray);
+  newStatsArray.push(['Total', totalRoles]);
+  return newStatsArray;
+};
+
+export const setWerewolfSideRolesStats = (keyArray, stats) => {
+  let newStatsArray = {};
+  let totalRoles = 0;
+  keyArray.forEach((key) => {
+    if (key === 'werewolf') {
+      newStatsArray['Le Loup-Garou'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'big_bad_wolf') {
+      newStatsArray['Le Grand Méchant Loup'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'father_of_wolves') {
+      newStatsArray["L'Infect Père des Loups"] = stats[key];
+      totalRoles += stats[key];
+    }
+    delete newStatsArray[key];
+  });
+  newStatsArray = Object.entries(newStatsArray);
+  newStatsArray.push(['Total', totalRoles]);
+  return newStatsArray;
+};
+
+export const setSoloSideRolesStats = (keyArray, stats) => {
+  let newStatsArray = {};
+  let totalRoles = 0;
+  keyArray.forEach((key) => {
+    if (key === 'pied_piper') {
+      newStatsArray['Le Joueur de Flûte'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'angel') {
+      newStatsArray["L'Ange"] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'white_werewolf') {
+      newStatsArray['Le Loup-Garou Blanc'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'sectarian') {
+      newStatsArray["L'Abominable Sectaire"] = stats[key];
+      totalRoles += stats[key];
+    }
+    delete newStatsArray[key];
+  });
+  newStatsArray = Object.entries(newStatsArray);
+  newStatsArray.push(['Total', totalRoles]);
+  return newStatsArray;
+};
+
+export const setAmbiguousSideRolesStats = (keyArray, stats) => {
+  let newStatsArray = {};
+  let totalRoles = 0;
+  keyArray.forEach((key) => {
+    if (key === 'thief') {
+      newStatsArray['Le Voleur'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'devoted_servant') {
+      newStatsArray['La Servante Dévouée'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'actor') {
+      newStatsArray['Le Comédien'] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'wild_child') {
+      newStatsArray["L'Enfant Sauvage"] = stats[key];
+      totalRoles += stats[key];
+    }
+    else if (key === 'dog_wolf') {
+      newStatsArray['Le Chien-Loup'] = stats[key];
+      totalRoles += stats[key];
+    }
+    delete newStatsArray[key];
+  });
+  newStatsArray = Object.entries(newStatsArray);
+  newStatsArray.push(['Total', totalRoles]);
+  return newStatsArray;
+};
+
+export const setOtherRolesStats = (keyArray, stats) => {
+  let newStatsArray = {};
+  keyArray.forEach((key) => {
+    if (key === 'captain') {
+      newStatsArray['Le Capitaine'] = stats[key];
+    }
+    else if (key === 'lover') {
+      newStatsArray['Les Amoureux'] = stats[key];
+    }
+    delete newStatsArray[key];
+  });
+  newStatsArray = Object.entries(newStatsArray);
+  return newStatsArray;
+};
+
+export const setChartData = (array, number) => {
+  const chartData = array.map((currentArray) => {
+    const dataObject = {};
+    const trueArrayLength = currentArray.length - 1;
+    if (trueArrayLength > 15) {
+      dataObject.x = 'Village';
+      dataObject.color = '0066cc';
+    }
+    else if (trueArrayLength === 3) {
+      dataObject.x = 'Loup-Garou';
+      dataObject.color = '8bc1f7';
+    }
+    else if (trueArrayLength === 4) {
+      dataObject.x = 'Solitaire';
+      dataObject.color = '519de9';
+    }
+    else if (trueArrayLength === 5) {
+      dataObject.x = 'Ambigü';
+      dataObject.color = '002f5d';
+    }
+    dataObject.y = (currentArray[trueArrayLength][1] * 100) / number;
+    return dataObject;
+  });
+  return chartData;
+};
